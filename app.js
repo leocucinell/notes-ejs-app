729dd5585f57d70e28b5d34107a5366b19cceb2a
LeoCucinell <leocucinell@gmail.com>
2020-12-30 19:09:26 -0800

    finished user adding items

--- app.js
+++ app.js
@@ -6,11 +6,12 @@ const app = express();
 
 app.set("view engine", 'ejs');
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 
-const sessionNotes = ["First note"];
+const sessionNotes = [];
 
 app.get("/", (req, res) => {
     const date = new Date();
@@ -18,12 +19,17 @@ app.get("/", (req, res) => {
     const month = date.getMonth();
     const day = date.getDate();
 
-    res.render("home", {today: weekdays[today], month: months[month], day: day});
+    res.render("home", {today: weekdays[today], month: months[month], day: day, sessionNotes: sessionNotes});
 });
+
 
 app.post("/", (req, res) => {
     //add a new component to the sessionNotes and re-render the home screen
-})
+    const newNote = req.body.note;
+    sessionNotes.push(newNote);
+    res.redirect("/");
+});
+
 
 app.listen(3000, () => {
     console.log("Server initialized on port 3k");
